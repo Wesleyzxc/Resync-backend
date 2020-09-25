badc0ff69ec5e0a69f9c9e5b1098574d6e403399
Wesley <wesleykok-@hotmail.com>
2020-09-25 20:55:31 +1000

    add submission date to organisation

--- routes/organisation.js
+++ routes/organisation.js
@@ -3,36 +3,47 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/org/select", function (req, res, next) {
-  let results = [];
-  req.db
-    .select()
-    .from("organisations")
-    .then((rows) => {
-      if (rows.length === 0) {
-        throw new Error("No organisations added yet.");
-      }
-      rows.map((row) => {
-        results.push({
-          id: row.id,
-          organisation_name: row.organisation_name,
-          owner: row.owner,
-          address: row.address,
-          city: row.city,
-          state: row.state,
-          country: row.country,
-        });
-      });
-      return res.status(200).json({ results });
-    })
-    .catch((err) => {
-      if (err.toString().includes("No organisations added yet.")) {
-        return res.status(200).json({ query: req.query, results: [] });
-      } else
-        return res.status(500).json({
-          error:
-            "oh no! It looks like there was a database error while performing your search, give it another try...",
-        });
+  if (!req.headers["authorization"])
+    return res.status(401).send({
+      error: "Missing the authorization header",
     });
+  let token = req.headers["authorization"].replace("Bearer ", "");
+  let results = [];
+  req.jwt.verify(token, process.env.secret, function (err) {
+    if (err) {
+      return res.status(401).send({ error: "Invalid token" });
+    } else {
+      req.db
+        .select()
+        .from("organisations")
+        .then((rows) => {
+          if (rows.length === 0) {
+            throw new Error("No organisations added yet.");
+          }
+          rows.map((row) => {
+            results.push({
+              id: row.id,
+              organisation_name: row.organisation_name,
+              owner: row.owner,
+              address: row.address,
+              city: row.city,
+              state: row.state,
+              country: row.country,
+              submission_date: row.submission_date,
+            });
+          });
+          return res.status(200).json({ results });
+        })
+        .catch((err) => {
+          if (err.toString().includes("No organisations added yet.")) {
+            return res.status(200).json({ query: req.query, results: [] });
+          } else
+            return res.status(500).json({
+              error: "oh no! It looks like there was a database error while performing your search, give it another try...",
+            });
+        });
+    }
+  });
 });
 
 router.post("/org/add", function (req, res, next) {
@@ -48,9 +59,7 @@ router.post("/org/add", function (req, res, next) {
     });
   let token = req.headers["authorization"].replace("Bearer ", "");
   if (org_name == undefined || owner == undefined) {
-    return res
-      .status(400)
-      .send({ error: "Missing organisation name or owner name" });
+    return res.status(400).send({ error: "Missing organisation name or owner name" });
   }
   req.jwt.verify(token, process.env.secret, function (err) {
     if (err) {
@@ -83,10 +92,7 @@ router.post("/org/add", function (req, res, next) {
 router.put("/org/update", function (req, res, next) {
   // old values to find in db
   let org_name = req.headers.organisation_name;
-  if (org_name == undefined)
-    return res
-      .status(400)
-      .send({ message: "No organisation provided to update." });
+  if (org_name == undefined) return res.status(400).send({ message: "No organisation provided to update." });
 
   // new values to update
   let new_OrgName = req.headers.new_organisation_name;
@@ -95,14 +101,7 @@ router.put("/org/update", function (req, res, next) {
   let city = req.headers.city;
   let state = req.headers.state;
   let country = req.headers.country;
-  if (
-    new_OrgName == undefined &&
-    newOwner == undefined &&
-    address == undefined &&
-    city == undefined &&
-    state == undefined &&
-    country == undefined
-  )
+  if (new_OrgName == undefined && newOwner == undefined && address == undefined && city == undefined && state == undefined && country == undefined)
     return res.status(400).send({ message: "No values provided to update." });
 
   if (!req.headers["authorization"])
@@ -129,19 +128,14 @@ router.put("/org/update", function (req, res, next) {
           country: country,
         })
         .then((data) => {
-          if (data == 0)
-            return res
-              .status(400)
-              .send({ message: "This entry does not exist." });
+          if (data == 0) return res.status(400).send({ message: "This entry does not exist." });
           res.status(200).send({
             message: "Saved changes",
           });
         })
         .catch((err) => {
           console.log(err);
-          return res
-            .status(400)
-            .send({ message: "The database cannot update this change." });
+          return res.status(400).send({ message: "The database cannot update this change." });
         });
     }
   });
